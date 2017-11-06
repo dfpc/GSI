f232c4cbeb1a90ae443b8c9e1617b7144ec5a331
dfpc <dfpc@users.noreply.github.com>
2017-11-06 21:48:06 +0000

    Registar Utilizador

--- BSP_Application/BSP_Application/DataObjects/AdicionarRegistos.cs
+++ BSP_Application/BSP_Application/DataObjects/AdicionarRegistos.cs
@@ -14,5 +14,14 @@ namespace BSP_Application.DataObjects
                 db.Database.ExecuteSqlCommand("exec spCreateProject @Nome ={0}, @Descricao={1}", "teste", "descrição teste");
             }
         }
+
+        public static void InsertUser(string username, string password, string email, string name)
+        {
+            using (DataBaseConnect db = new DataBaseConnect())
+            {
+                db.Database.ExecuteSqlCommand("INSERT INTO Utilizador (Username, Password, Email, Nome, IsAdmin) VALUES({0}, {1}, {2}, {3}, 0)", 
+                    username, password, email, name);
+            }
+        }
     }
 }
