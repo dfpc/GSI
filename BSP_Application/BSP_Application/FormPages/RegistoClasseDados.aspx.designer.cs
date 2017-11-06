f7a1d143247ce16be478afe3b74c10f94a190339
solangepaz <paz_solange@hotmail.com>
2017-11-06 23:25:15 +0000

    registar classe
    incompleto!!

--- BSP_Application/BSP_Application/FormPages/RegistoClasseDados.aspx.designer.cs
+++ BSP_Application/BSP_Application/FormPages/RegistoClasseDados.aspx.designer.cs
@@ -20,5 +20,41 @@ namespace BSP_Application.FormPages {
         /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
         /// </remarks>
         protected global::BSP_Application.UserControls.MainMenu MainMenu1;
+        
+        /// <summary>
+        /// Controle ListaProjetos.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ListaProjetos;
+        
+        /// <summary>
+        /// Controle inputNome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText inputNome;
+        
+        /// <summary>
+        /// Controle comment.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTextArea comment;
+        
+        /// <summary>
+        /// Controle registarclasse.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button registarclasse;
     }
 }
