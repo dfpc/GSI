7c7561c868d940cf8bbb039a6fe36420cc04c781
dfpc <dfpc@users.noreply.github.com>
2017-11-06 23:39:06 +0000

    km

--- BSP_Application/BSP_Application/FormPages/RegistoClasseDados.aspx.cs
+++ BSP_Application/BSP_Application/FormPages/RegistoClasseDados.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -12,12 +13,35 @@ namespace BSP_Application.FormPages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\BSP_DataBase.mdf;Integrated Security=True");
+            string com = "Select Nome, IDProjeto from Projeto";
+            SqlDataAdapter adpt = new SqlDataAdapter(com, conn);
+            DataTable dt = new DataTable();
+            adpt.Fill(dt);
+            ListaProjetos.DataSource = dt;
+          //  ListaProjetos.DataBind();
+            ListaProjetos.DataTextField = "Nome";
+            ListaProjetos.DataValueField = "IDProjeto";
+            ListaProjetos.DataBind();
         }
 
 
-        public void Guardar_Classe(object sender, EventArgs e)
+
+        protected void Guardar_ClasseDados(object sender, EventArgs e)
+
         {
+        
+            string nome = inputNome.Value;
+            string descricao = comment.Value;
+            int idprojeto = Int32.Parse(ListaProjetos.SelectedValue);
+            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\BSP_DataBase.mdf;Integrated Security=True");
+            string sql = "INSERT INTO ClasseDados (Nome, Descricao, IDProjeto) values (@nome, @descricao, @idprojeto)";
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@nome", nome);
+            cmd.Parameters.AddWithValue("@descricao", descricao);
+            cmd.Parameters.AddWithValue("@idprojeto", idprojeto);
+            cmd.ExecuteNonQuery();
         }
     }
 }
